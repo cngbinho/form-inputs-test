58263ada6176e69a262729920a90a183fd4afcff
cngbinho <robson.sousa21@gmail.com>
2020-01-26 12:12:38 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'projeto-teste';
+  title = "projeto-teste";
+
+  arrFields: any = [0, 1, 2];
+  arrColumns: any = [0, 1, 2];
+  fieldsValue: any = [];
+  totalValue: any = [0, 0, 0];
+  geralTotal = 0;
+
+  showLog(event?, field?) {
+    this.fieldsValue[field] = event;
+
+    this.sumTotal();
+  }
+
+  sumTotal() {
+    this.totalValue[0] = 0;
+    this.totalValue[1] = 0;
+    this.totalValue[2] = 0;
+
+    Object.keys(this.fieldsValue).map(item => {
+      this.totalValue[Number(item.split("-")[1])] += this.fieldsValue[item];
+    });
+
+    this.geralTotal = this.totalValue
+      .map(item => item)
+      .reduce((curValue = 0, nextValue) => (curValue += nextValue));
+  }
 }
